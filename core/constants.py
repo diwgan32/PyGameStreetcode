bca9e3a1785a04e614f0c2d9c35961d0898d5139
Zahir Bokhari <syedzahirbokhari@gmail.com>
2018-03-24 21:50:52 -0700

    Cleanup files a bit
    Split out Screen.py into Sprite.py and constants.py.
    Screen is still the only thing students need to import, because all
    object creation is done through the screen, but this should make things a bit
    better organized

---
+++ core/constants.py
@@ -0,0 +1,11 @@
+from pygame_util import *
+
+SCREEN_SIZE = 640, 480
+BLACK = T(0, 0, 0)
+ORANGE = T(255, 165, 0)
+PINK = T(255, 192, 203)
+RED = T(240, 10, 10)
+BLUE = T(0, 0, 255)
+YELLOW = T(255, 255, 0)
+WHITE = T(255, 255, 255)
+
